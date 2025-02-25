dc7edcb4bba736d37fb6fe531ea4c6b05250d095
ormr11 <alexanmateosyan@gmail.com>
2025-02-25 19:13:33 +0200

    Revert "initial commit"
    This reverts commit f32a3f0cd0eeb88c9510e16eb3b797b8e0042c04.

--- src/main/java/com/officerental/rental/Services/IUserService.java
+++
@@ -1,9 +0,0 @@
-package com.officerental.rental.Services;
-
-
-import com.officerental.rental.models.User;
-
-public interface IUserService {
-    User registerUser(User user);
-    User loginUser(String email, String password);
-}
